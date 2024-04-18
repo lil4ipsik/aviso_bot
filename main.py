c1ef97a945693c95ed28062ba6b7cc712f6039a5
xxanqw <potik217@gmail.com>
2024-04-18 20:14:02 +0300

    Nuitka build properties

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+########################################################################################
+#      Nuitka Configuration (DO NOT CHANGE IF YOU DON'T KNOW WHAT YOU ARE DOING)       
+#                                                                                      
+# nuitka-project: --onefile --include-data-files=windows/icon.ico=windows/icon.ico     
+# nuitla-project: --deployment --disable-console --enable-plugin=pyside6               
+# nuitka-project: --output-dir=build --product-name="Aviso Bot"                        
+# nuitka-project: --windows-icon-from-ico=windows/icon.ico --product-version=0.0.0.4   
+# nuitka-project: -o "aviso bot"                                                       
+########################################################################################
+
 import sys
 
 from PySide6.QtWidgets import QApplication
