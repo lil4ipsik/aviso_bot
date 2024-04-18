dddf38bc83a5d3bc1d8f8f211c827b0e033d80f1
xxanqw <potik217@gmail.com>
2024-04-18 20:39:21 +0300

    Corrected nuitka tags

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 ########################################################################################
 #      Nuitka Configuration (DO NOT CHANGE IF YOU DON'T KNOW WHAT YOU ARE DOING)       
-#                                                                                      
-# nuitka-project: --onefile --include-data-files=windows/icon.ico=windows/icon.ico     
-# nuitla-project: --deployment --disable-console --enable-plugin=pyside6               
-# nuitka-project: --output-dir=build --product-name="Aviso Bot"                        
-# nuitka-project: --windows-icon-from-ico=windows/icon.ico --product-version=0.0.0.4   
-# nuitka-project: -o "aviso bot"                                                       
+#                                                                                     
+# nuitka-project: --include-data-files={MAIN_DIRECTORY}/windows/icon.ico=windows/icon.ico     
+# nuitka-project: --deployment
+# nuitka-project: --disable-console
+# nuitka-project: --enable-plugin=pyside6               
+# nuitka-project: --output-dir=build
+# nuitka-project: --product-name="Aviso Bot"                        
+# nuitka-project: --windows-icon-from-ico={MAIN_DIRECTORY}/windows/icon.ico
+# nuitka-project: --product-version=0.0.0.4   
+# nuitka-project: -o "aviso bot"                                                    
 ########################################################################################
 
 import sys
