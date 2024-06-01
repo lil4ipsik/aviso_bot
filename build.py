863515c2516b6d9a8f4bfc66ca68377a650c64b6
xxanqw <potik217@gmail.com>
2024-06-01 18:02:42 +0300

    Spelling

--- build.py
+++ build.py
@@ -12,7 +12,7 @@ def main():
         os.system(f"nuitka --onefile --follow-imports --enable-plugin=pyqt6 -o aviso-bot-debug --output-dir=build --windows-icon-from-ico=./windows/icon.ico --include-data-files=./windows/icon.ico=windows/icon.ico --include-data-files=./res/version.json=res/version.json --deployment --company-name=Lil4ipsik --product-name=\"Aviso Bot (debug)\" --product-version={version(True)} main.py")
     else:
         print("Build with console disabled (default)")
-        os.system(f"nuitka --onefile --follow-imports --enable-plugin=pyqt6 -o aviso-bot-debug --disable-console --output-dir=build --windows-icon-from-ico=./windows/icon.ico --include-data-files=./windows/icon.ico=windows/icon.ico --include-data-files=./res/version.json=res/version.json --deployment --company-name=Lil4ipsik --product-name=\"Aviso Bot\" --product-version={version(True)} main.py")
+        os.system(f"nuitka --onefile --follow-imports --enable-plugin=pyqt6 -o aviso-bot --disable-console --output-dir=build --windows-icon-from-ico=./windows/icon.ico --include-data-files=./windows/icon.ico=windows/icon.ico --include-data-files=./res/version.json=res/version.json --deployment --company-name=Lil4ipsik --product-name=\"Aviso Bot\" --product-version={version(True)} main.py")
     
 if __name__ == "__main__":
     main()
