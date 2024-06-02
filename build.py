d8827d84905ce2310a87114425db9dc88430bc64
xxanqw <potik217@gmail.com>
2024-06-02 18:05:08 +0300

    Linux build HOTFIX

--- build.py
+++ build.py
@@ -12,7 +12,7 @@ def main():
         os.system(f"nuitka --onefile --follow-imports --enable-plugin=pyqt6 -o aviso-bot-debug --output-dir=build --windows-icon-from-ico=./windows/icon.ico --include-data-files=./windows/icon.ico=windows/icon.ico --include-data-files=./res/version.json=res/version.json --deployment --company-name=Lil4ipsik --product-name=\"Aviso Bot (debug)\" --product-version={version(True)} main.py")
     elif "--linux" in parameters:
         print("Build for Linux")
-        os.system(f"nuitka --onefile --follow-imports --enable-plugin=pyqt6 -o aviso-bot --output-dir=build --linux-onefile-icon=./windows/icon.ico --include-data-files=./linux/icon.png=linux/icon.png --include-data-files=./res/version.json=res/version.json --deployment --company-name=Lil4ipsik --product-name=\"Aviso Bot\" --product-version={version(True)} main.py")
+        os.system(f"python -m nuitka --onefile --follow-imports --enable-plugin=pyqt6 -o aviso-bot --output-dir=build --linux-onefile-icon=./windows/icon.ico --include-data-files=./linux/icon.png=linux/icon.png --include-data-files=./res/version.json=res/version.json --deployment --company-name=Lil4ipsik --product-name=\"Aviso Bot\" --product-version={version(True)} main.py")
     else:
         print("Build with console disabled (default)")
         os.system(f"nuitka --onefile --follow-imports --enable-plugin=pyqt6 -o aviso-bot --disable-console --output-dir=build --windows-icon-from-ico=./windows/icon.ico --include-data-files=./windows/icon.ico=windows/icon.ico --include-data-files=./res/version.json=res/version.json --deployment --company-name=Lil4ipsik --product-name=\"Aviso Bot\" --product-version={version(True)} main.py")
