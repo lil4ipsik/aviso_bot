a91a3039fdada8a23b6a1c209da042df53bcc8e4
xxanqw <potik217@gmail.com>
2024-06-12 17:51:05 +0300

    Theme

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from PyQt6.QtWidgets import QApplication
 from windows.main_window import MainWindow
 
 app = QApplication([])
+app.setStyle('Fusion')
 window = MainWindow()
 window.show()
 sys.exit(app.exec())
